ef621576bc09709141a231985d340fa28d5c08d8
0xardy <ardy@wonderland.xyz>
2025-12-02 11:12:57 -0300

    chore: remove redundant comment

--- test/provider.spec.ts
+++ test/provider.spec.ts
@@ -15,7 +15,6 @@ const mockChain = {
 
 // Anvil's first test private key
 const TEST_PRIVATE_KEY: Hex = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-// Corresponding address: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 const TEST_ADDRESS: Address = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 
 const baseConfig: E2EProviderConfig = {
